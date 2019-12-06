28fd87b50d3638562fa0a5b6a032b71ba9a4c275
wendko <wendko.88@gmail.com>
2019-12-06 12:42:33 +0800

    1 file modified

--- index.ts
+++ index.ts
@@ -7,82 +7,184 @@ function rxjsSimpleExample() {
    * Observer that unsubscribes after a while
    */
 
+
   /**
    * Create an observable that tracks every second
    * https://rxmarbles.com/#interval
    */
 
+
+
+
+
+
+
+  const observable = interval(1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  /**
    * The observer should return a marble 🔵 every second
    */
 
-  /**
-   * The observer should stop subscribing after 5 seconds
-   */
-}
 
-function rxjsSimpleExample2(){
-  /**
-   * Observer outputs different stuff depending on the value observed
-   */
-  const observable = interval(1000);
+
+
+
+
 
   const observer = observable
   .pipe(
-    /**
-     * Output a blue 🔵 marble if it's an odd second
-     * A red 🔴 marble if it's an even second
-     */
-  ).subscribe();
 
-  setTimeout(()=> observer.unsubscribe(), 5000);
-}
 
-function rxjsSimpleExample3(){
+    tap( val => console.log(val, '🔵'))
+
+
+  );
+
+
+
+
+
+
+
+
+  const subscription = observer.subscribe();
+
+
+
+
+
+
+
+
+
+
   /**
-   * Make it such that the observer doesn't need to unsubscribe
-   * hint: take, map
+   * The observer should stop subscribing after a while
    */
-  const observable = interval(1000);
 
-  observable
-  .pipe(
-    /**
-     * Same output as example 2
-     * Except this time no explicit unsubscribe
-     */
-    tap(() => console.log('🔵'))
-  ).subscribe();
+
+
+
+
+
+
+
+
+  setTimeout(() => subscription.unsubscribe(), 6000);
+
+
+
+
+
+
+
+
+
+
 }
+
+rxjsSimpleExample();
+
+
+
+
+
+
+
+
+
+
+
+
 
 function rxjsExampleMarbleClicked() {
   /* Observer to observe user action instead of interval
-  * class name: marble-click
-  * hint: fromEvent
-  * https://rxmarbles.com/#map
   */
-  const observable = EMPTY;
+
+
+
+
+
+
+  const observable = fromEvent(document.getElementsByClassName('marble-click'), 'click');
+
+
+
+
+
+
+
+
 
   observable.pipe(
+
+    /** click event */
     // tap(console.log),
-    map(event => (event.target as HTMLDivElement).innerText),
-    tap(marble => {
-      const countElement = document.getElementById(marble === '🔴' ? 'redCount' : 'blueCount');
-      countElement.innerText = `${+countElement.innerText + 1}`;
-    })
+
+
+    /** div inner text */
+    // map(event => (event.target as HTMLDivElement).innerText),
+
+
+    /** update count */
+    // tap(marble => {
+    //   const countElement = document.getElementById(marble === '🔴' ? 'redCount' : 'blueCount');
+    //   countElement.innerText = `${+countElement.innerText + 1}`;
+    // })
+
+
   ).subscribe();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 function rxjsExampleMarbleThrottle() {
   /**
    * Throttle user input
-   * marble: debounceTime
-   * https://rxmarbles.com/#debounceTime
    */
+
+
+
+
+
   const observable = fromEvent(document.getElementById('marbleTextInput'), 'input');
 
+
+
+
+
+
+
   observable.pipe(
+
+    // debounceTime(500),
+
     map(event => (event.target as HTMLInputElement).value),
+
     tap((marbleText: string) => {
       const outputElement = document.getElementById('marbleTextOutput');
       outputElement.innerHTML = Array.from(marbleText).reduce(
@@ -92,16 +194,39 @@ function rxjsExampleMarbleThrottle() {
         ''
       );
     }),
+
+    // debounceTime(500),
+
   ).subscribe();
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
 function rxjsExampleMarbleSwitch() {
-  /**
-   * https://rxmarbles.com/#switchMap
-   */
+
+
+
 
     const observable = fromEvent(document.getElementsByClassName('marble-switch'), 'click');
+
+
+
+
+
+
+
+
 
     const timeObservable = interval(1000)
     .pipe(
